1c50265e6f01e713c0c78a620f2237a2c58fd983
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-16 17:16:55 -0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,11 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json("Working");
+});
+
+module.exports = router;
+
 // Router for the home page
 // Show the calendar
 // Create a new task
