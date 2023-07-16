f0f2003a6be0c77260c618f3855aff8f523e1599
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-16 17:22:14 -0300

    User model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, require: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+modules.exports = mongoose.model("User", UserSchema);
+
 // models for user
