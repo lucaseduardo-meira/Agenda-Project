4e45d23d9d7bc89ded0c771c7f6ca11b02d2351c
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-16 17:12:46 -0300

    mongoose

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Working");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 
