2b9d87efde05f950ab594fc28ef306715be39a64
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-16 18:01:04 -0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -8,6 +8,21 @@ module.exports = router;
 
 // Router for the home page
 // Show the calendar
+router.get("/", (req, res) => {
+  res.status(200).json("Working");
+});
+router.get("/", (req, res) => {
+  res.status(200).json("Working");
+});
+
 // Create a new task
+
+router.post("/", (req, res) => {});
+
 // Update a task
+
+router.put("/", (req, res) => {});
+
 // Delete a task
+
+router.delete("/", (req, res) => {});
