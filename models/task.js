4d6062010313bc0ff3b8162b7c538d64a7dbcafc
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-17 19:22:10 -0300

    Update task.js

--- models/task.js
+++ models/task.js
@@ -2,4 +2,10 @@ const mongoose = require("mongoose");
 
 // Models for the task
 
+const TaskSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  description: { type: String },
+});
+
 //Task terá titulo, horario e descriçao
