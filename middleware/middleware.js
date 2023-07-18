fae64483241cbadd0a058cfc53cc503fc405af0b
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-18 20:02:36 -0300

    Update middleware.js

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
       next();
     });
   } else {
-    return res.status(401).json("Token was not found");
+    return res.redirect("/login");
   }
 };
 
