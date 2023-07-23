968fea50a4993b634a4089c9202688952d5d7b7c
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-23 13:47:57 -0300

    Create render.js

---
+++ src/services/render.js
@@ -0,0 +1,11 @@
+exports.login = (req, res) => {
+  res.render("login");
+};
+
+exports.register = (req, res) => {
+  res.render("register");
+};
+
+exports.home = (req, res) => {
+  res.render("index");
+};
