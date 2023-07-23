9ee9420c2a465be8018a03f5d9fdb10036ce7c8f
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-23 13:44:28 -0300

    Update server.js

--- src/database/server.js
+++ src/database/server.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose");
 
 mongo_uri = process.env.MONGO_URL || "mongodb://localhost/agenda";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(mongo_uri);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+mongoose.Promise = global.Promise;
+
+module.exports = { mongoose, connectDB };
