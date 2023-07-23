edfb706706d49feac32d0db7e27bbeb18ecf4b49
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-23 14:19:12 -0300

    css and img fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connectDB } = require("./src/database/server");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const path = require("path");
 // const loginRouter = require("./routes/login");
 // const homeRouter = require("./routes/home");
 
@@ -11,6 +12,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 
 app.set("view engine", "ejs");
 
