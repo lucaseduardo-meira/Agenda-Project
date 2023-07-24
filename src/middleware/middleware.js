7ab08802f75bc6c25683a98cd0150ebbd5f6ce04
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-24 20:09:33 -0300

    Update middleware.js

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) return res.status(403).json("Token is not valid");
-      req.user = user;
-      next();
-    });
-  } else {
+  const token = req.cookies.access_token;
+
+  if (!token) {
     return res.redirect("/login");
+  }
+  try {
+    const user = jwt.verify(token, process.env.JWT_SEC);
+    req.user = user;
+    return next();
+  } catch (error) {
+    console.log(error);
   }
 };
 
