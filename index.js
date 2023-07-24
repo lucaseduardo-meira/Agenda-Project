e082cf255ec9a176e33d3447acb7c65a183b9df9
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-24 19:57:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connectDB } = require("./src/database/server");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 // const loginRouter = require("./routes/login");
 // const homeRouter = require("./routes/home");
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
