4887ea0d77c5a55394857a247fe0ddfb1503962a
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-26 20:13:55 -0300

    Update App.js

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
 import "./App.css";
 import { getMonth } from "./util";
 
 function App() {
   console.table(getMonth());
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <div className="h-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <SideBar />
+          <Month />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
