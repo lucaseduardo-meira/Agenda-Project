9ec300a3ccf824c387567a43fed37067a1e12231
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-26 21:14:11 -0300

    day and month componets

--- front_end/src/components/Month.js
+++ front_end/src/components/Month.js
@@ -4,7 +4,11 @@ export default function Month({ month }) {
   return (
     <div className="flex-1 grid grid-cols-7 grid-rows-5">
       {month.map((row, i) => (
-        <React.Fragment></React.Fragment>
+        <React.Fragment key={i}>
+          {row.map((day, idx) => (
+            <Day day={day} key={idx} />
+          ))}
+        </React.Fragment>
       ))}
     </div>
   );
