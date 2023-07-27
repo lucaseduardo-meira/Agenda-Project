7cc05ccefb6a5346513108aeccfdf8e12e889e75
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-26 21:12:59 -0300

    Update Month.js

--- front_end/src/components/Month.js
+++ front_end/src/components/Month.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Month() {
-  return <div>Month</div>;
+export default function Month({ month }) {
+  return (
+    <div className="flex-1 grid grid-cols-7 grid-rows-5">
+      {month.map((row, i) => (
+        <React.Fragment></React.Fragment>
+      ))}
+    </div>
+  );
 }
