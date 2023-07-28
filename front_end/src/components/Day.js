5a73d3e86911452d8d80dc5ef2a49ff78a718ae0
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-28 18:51:06 -0300

    Update Day.js

--- front_end/src/components/Day.js
+++ front_end/src/components/Day.js
@@ -1,6 +1,12 @@
+import dayjs from "dayjs";
 import React from "react";
 
 export default function Day({ day, rowIdx }) {
+  function getCurrentDayClass() {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "";
+  }
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -8,7 +14,9 @@ export default function Day({ day, rowIdx }) {
           <p className="text-sm mt-1 ">{day.format("ddd").toUpperCase()}</p>
         )}
 
-        <p className="text-sm p-1 my-1 text-center">{day.format("DD")}</p>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+          {day.format("DD")}
+        </p>
       </header>
     </div>
   );
