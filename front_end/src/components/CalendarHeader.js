36209908a097cb3515718adeb941565fde532c87
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-28 19:14:52 -0300

    Update CalendarHeader.js

--- front_end/src/components/CalendarHeader.js
+++ front_end/src/components/CalendarHeader.js
@@ -6,6 +6,16 @@ export default function CalendarHeader() {
     <div>
       <header className="px-4 py-2 flex items-center">
         <img src={logo} alt="logo" className="mr-2 w-12 h-12" />
+        <h1 className="mr-10 text-xl text-gray-500 fond-bold">Calendar</h1>
+        <button className="border rounded py-2 px-4 mr-5">Today</button>
+        <button>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+          </span>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+          </span>
+        </button>
       </header>
     </div>
   );
