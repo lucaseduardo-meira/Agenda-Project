aafed6550162090a408c6f4fd18a77f119e9eec7
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-29 20:50:42 -0300

    Handling the prev and next month

--- front_end/src/context/ContextWrapper.js
+++ front_end/src/context/ContextWrapper.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import GlobalContext from "./GlobalContext";
+import dayjs from "dayjs";
 
-export default function ControxtWrapper() {
-  return <div>ControxtWrapper</div>;
+export default function ContextWrapper(props) {
+  const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  return (
+    <GlobalContext.Provider value={{ monthIndex, setMonthIndex }}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
 }
