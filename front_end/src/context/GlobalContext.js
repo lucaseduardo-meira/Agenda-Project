094086ba6fe16272f962002840e8d95cb58815e1
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-29 20:34:15 -0300

    Update GlobalContext.js

--- front_end/src/context/GlobalContext.js
+++ front_end/src/context/GlobalContext.js
@@ -2,5 +2,7 @@ import React from "react";
 
 const GlobalContext = React.createContext({
   monthIndex: 0,
-  setMonthIndex: () => {},
+  setMonthIndex: (index) => {},
 });
+
+export default GlobalContext;
