82e8cbfbdbb50ea06f4cb45241337bf933b1db42
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-30 17:43:10 -0300

    Update CalendarHeader.js

--- front_end/src/components/CalendarHeader.js
+++ front_end/src/components/CalendarHeader.js
@@ -12,7 +12,11 @@ export default function CalendarHeader() {
     setMonthIndex(monthIndex + 1);
   }
   function handleReset() {
-    setMonthIndex(dayjs().month());
+    setMonthIndex(
+      monthIndex === dayjs().month()
+        ? monthIndex + Math.random()
+        : dayjs().month()
+    );
   }
   return (
     <div>
