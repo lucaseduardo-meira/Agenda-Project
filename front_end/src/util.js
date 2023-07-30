7b90f761911ed764da87d0713e46df3e3bc90549
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-30 17:31:50 -0300

    Update util.js

--- front_end/src/util.js
+++ front_end/src/util.js
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 
 export function getMonth(month = dayjs().month()) {
   const year = dayjs().year();
-  const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
+  const firstDayOfTheMonth = dayjs(new Date(year, month, -1)).day();
   let currentMonthCount = 0 - firstDayOfTheMonth;
   const daysMatrix = new Array(5).fill([]).map(() => {
     return new Array(7).fill(null).map(() => {
