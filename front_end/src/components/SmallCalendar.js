f32983843b39f84c6ef6db93704422256cdef1d2
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-30 17:41:33 -0300

    Update SmallCalendar.js

--- front_end/src/components/SmallCalendar.js
+++ front_end/src/components/SmallCalendar.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { getMonth } from "../util";
+import GlobalContext from "../context/GlobalContext";
 
 export default function SmallCalendar() {
   const [currentMonthIdx, setCurrentMonthIdx] = useState(dayjs().month());
@@ -8,12 +9,35 @@ export default function SmallCalendar() {
   useEffect(() => {
     setCurrentMonth(getMonth(currentMonthIdx));
   }, [currentMonthIdx]);
+
+  const { monthIndex } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonthIdx(monthIndex);
+  }, [monthIndex]);
+
+  function handlePrevMonth() {
+    setCurrentMonthIdx(currentMonthIdx - 1);
+  }
+  function handleNextMonth() {
+    setCurrentMonthIdx(currentMonthIdx + 1);
+  }
   return (
     <div className="mt-9">
       <header className="flex justify-between">
         <p className="text-gray-500 font-bold">
           {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
         </p>
+        <button onClick={handlePrevMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+          </span>
+        </button>
+        <button onClick={handleNextMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+          </span>
+        </button>
       </header>
     </div>
   );
