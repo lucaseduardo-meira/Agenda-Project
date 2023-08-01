1bce70a93f003c6c3a7fd09dd3ddef5ac079c093
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-01 18:09:59 -0300

    colors in the event model

--- front_end/src/components/EventModel.js
+++ front_end/src/components/EventModel.js
@@ -1,8 +1,10 @@
 import React, { useContext, useState } from "react";
 import GlobalContext from "../context/GlobalContext";
+const labelsClasses = ["indigo", "gray", "green", "blue", "red", "purple"];
 
 export default function EventModel() {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const { setShowEventModel, daySelected } = useContext(GlobalContext);
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
@@ -34,6 +36,35 @@ export default function EventModel() {
               schedule
             </span>
             <p>{daySelected.format("dddd, MMMM DD")}</p>
+            <span className="material-icons-outlined text-gray-400">
+              segment
+            </span>
+            <input
+              type="text"
+              name="description"
+              placeholder="Add a description"
+              value={description}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              bookmark_border
+            </span>
+            <div className="flex gap-x-2">
+              {labelsClasses.map((lblClass, i) => (
+                <span
+                  key={i}
+                  className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursos-pointer`}
+                >
+                  <span className="material-icons-outlined text-white text-sm">
+                    check
+                  </span>
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </form>
