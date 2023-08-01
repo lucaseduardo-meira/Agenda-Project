7f44e150db0f4ed5a259b0197725576f79485adf
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-07-31 23:08:38 -0300

    Update EventModel.js

--- front_end/src/components/EventModel.js
+++ front_end/src/components/EventModel.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
 
 export default function EventModel() {
+  const { setShowEventModel } = useContext(GlobalContext);
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
@@ -8,7 +10,7 @@ export default function EventModel() {
           <span className="matirial-icons-outlined text-gray-400">
             drag_handle
           </span>
-          <button>
+          <button onClick={() => setShowEventModel(false)}>
             <span className="matirial-icons-outlined text-gray-400">close</span>
           </button>
         </header>
