25be9b252ecebb00f781e935817fe4ca8a18b7c6
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-01 17:33:31 -0300

    Update EventModel.js

--- front_end/src/components/EventModel.js
+++ front_end/src/components/EventModel.js
@@ -8,11 +8,11 @@ export default function EventModel() {
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
         <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
-          <span className="matirial-icons-outlined text-gray-400">
+          <span className="material-icons-outlined text-gray-400">
             drag_handle
           </span>
           <button onClick={() => setShowEventModel(false)}>
-            <span className="matirial-icons-outlined text-gray-400">close</span>
+            <span className="material-icons-outlined text-gray-400">close</span>
           </button>
         </header>
 
