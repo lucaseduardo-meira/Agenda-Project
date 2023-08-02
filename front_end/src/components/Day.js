aff70477d623dfd0698d97891209f495b9920ee1
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-02 20:42:12 -0300

    showing the events

--- front_end/src/components/Day.js
+++ front_end/src/components/Day.js
@@ -1,15 +1,23 @@
 import dayjs from "dayjs";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import GlobalContext from "../context/GlobalContext";
 
 export default function Day({ day, rowIdx }) {
+  const [dayEvents, setDayEvents] = useState([]);
+  const { setDaySelected, setShowEventModel, savedEvents } =
+    useContext(GlobalContext);
+  useEffect(() => {
+    const events = savedEvents.filter(
+      (evt) => dayjs(evt.day).format("DD-MM-YY") === day.format("DD-MM-YY")
+    );
+    setDayEvents(events);
+  }, [savedEvents, day]);
+
   function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
       ? "bg-blue-600 text-white rounded-full w-7"
       : "";
   }
-  const { setDaySelected, setShowEventModel, savedEvents } =
-    useContext(GlobalContext);
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -28,7 +36,14 @@ export default function Day({ day, rowIdx }) {
           setShowEventModel(true);
         }}
       >
-        {""}
+        {dayEvents.map((evt, idx) => (
+          <div
+            key={idx}
+            className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+          >
+            {evt.title}
+          </div>
+        ))}
       </div>
     </div>
   );
