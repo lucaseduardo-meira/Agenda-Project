035b2abb59dfcbbce24886db1a8927f0c74ad4db
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-02 21:11:18 -0300

    Update ContextWrapper.js

--- front_end/src/context/ContextWrapper.js
+++ front_end/src/context/ContextWrapper.js
@@ -70,6 +70,8 @@ export default function ContextWrapper(props) {
         savedEvents,
         selectedEvent,
         setSelectedEvent,
+        setLabels,
+        labels,
       }}
     >
       {props.children}
