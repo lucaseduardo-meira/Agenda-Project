c7ca3fea9b60abd7cf48f45d205db7d8003b1495
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-14 20:29:46 -0300

    Delete script.js

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const nameInput = document.getElementsByName("usuario");
-const passwordInput = document.getElementsByName("senha");
-
-nameInput.addEventListener("blur", (e) => {
-  alert("HELLO");
-});
