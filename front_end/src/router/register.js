36de0c63060aa1a2690d4ede1951c32d746e618f
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-14 20:16:42 -0300

    Register css working

--- front_end/src/router/register.js
+++ front_end/src/router/register.js
@@ -1,9 +1,54 @@
 import React from "react";
+import "../assets/css/register.css";
 
 function Register() {
   return (
-    <div>
-      <h1>PAGINA DE REGISTER</h1>
+    <div class="main-login">
+      <form action="/register" method="post" class="card-login">
+        <div class="card-header">
+          <h1>
+            Inscreva-se <br />e planeje a sua agenda
+          </h1>
+        </div>
+        <hr />
+        <div class="textfield">
+          <label for="username">Usuário</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Usuário"
+            onfocus="this.placeholder = ''"
+            onblur="this.placeholder = 'Digite seu usuário'"
+          />
+        </div>
+        <div class="textfield">
+          <label for="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onfocus="this.placeholder = ''"
+            onblur="this.placeholder = 'Digite seu Email'"
+          />
+        </div>
+        <div class="textfield">
+          <label for="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Senha"
+            onfocus="this.placeholder = ''"
+            onblur="this.placeholder = 'Digite sua senha'"
+          />
+        </div>
+        <button type="submit" class="btn-login">
+          Registre-se
+        </button>
+        <hr class="hr2" />
+        <a href="/login" class="login">
+          Já tenho uma conta
+        </a>
+      </form>
     </div>
   );
 }
