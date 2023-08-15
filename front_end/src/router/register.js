eb9afdee7705026dfa8417ff78e4e5b2d676757d
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-15 18:57:49 -0300

    register css fix

--- front_end/src/router/register.js
+++ front_end/src/router/register.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "../assets/css/register.css";
+import "../assets/css/styles.css";
 
 function Register() {
   return (
-    <div class="main-login">
-      <form action="/register" method="post" class="card-login">
+    <div class="main-register">
+      <form action="/register" method="post" class="card-register">
         <div class="card-header">
           <h1>
             Inscreva-se <br />e planeje a sua agenda
           </h1>
         </div>
-        <hr />
-        <div class="textfield">
+        <hr class="hr1" />
+        <div class="textfield-register">
           <label for="username">Usuário</label>
           <input
             type="text"
@@ -21,7 +21,7 @@ function Register() {
             onblur="this.placeholder = 'Digite seu usuário'"
           />
         </div>
-        <div class="textfield">
+        <div class="textfield-register">
           <label for="email">Email</label>
           <input
             type="text"
@@ -31,7 +31,7 @@ function Register() {
             onblur="this.placeholder = 'Digite seu Email'"
           />
         </div>
-        <div class="textfield">
+        <div class="textfield-register">
           <label for="password">Senha</label>
           <input
             type="password"
@@ -41,11 +41,11 @@ function Register() {
             onblur="this.placeholder = 'Digite sua senha'"
           />
         </div>
-        <button type="submit" class="btn-login">
+        <button type="submit" class="btn-register">
           Registre-se
         </button>
         <hr class="hr2" />
-        <a href="/login" class="login">
+        <a href="/login" class="login-register">
           Já tenho uma conta
         </a>
       </form>
