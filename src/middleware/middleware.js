8d408a87248da48f0a61755fd29221dd2110a005
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-21 18:22:51 -0300

    Update middleware.js

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -3,7 +3,6 @@ const User = require("../models/user");
 
 const verifyToken = async (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization) {
     return res.status(401).json({ error: "Authorization token required" });
   }
