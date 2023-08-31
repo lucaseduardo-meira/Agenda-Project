869ca9620b81bb482decc56b65d22dd8321a1baf
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-08-31 18:49:10 -0300

    css

--- front_end/src/components/CreateEventButton.js
+++ front_end/src/components/CreateEventButton.js
@@ -7,10 +7,17 @@ export default function CreateEventButton() {
   return (
     <button
       onClick={() => setShowEventModel(true)}
-      className="border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl transition duration-150"
+      className="border border-[#191919] p-2 rounded-full flex items-center shadow-md hover:shadow-2xl transition duration-150 bg-[#191919]"
     >
-      <img src={plusImg} alt="create_event" className="w-7 h-7" />
-      <span className="pl-3 pr-7">Criar</span>
+      {/* <img src={plusImg} alt="create_event" className="w-7 h-7" /> */}
+      <svg width="36" height="36" viewBox="0 0 36 36">
+        <path fill="#34A853" d="M16 16v14h4V20z"></path>
+        <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+        <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+        <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+        <path fill="none" d="M0 0h36v36H0z"></path>
+      </svg>
+      <span className="pl-3 pr-7 text-[#afb3b5]">Criar</span>
     </button>
   );
 }
