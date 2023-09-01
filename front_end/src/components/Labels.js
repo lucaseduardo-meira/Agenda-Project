1af1362afb054287a0cc0117999d980cda4b1fb3
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-09-01 19:21:31 -0300

    Update Labels.js

--- front_end/src/components/Labels.js
+++ front_end/src/components/Labels.js
@@ -13,7 +13,7 @@ export default function Labels() {
   const { labels, updateLabel } = useContext(GlobalContext);
   return (
     <React.Fragment>
-      <p className="text-gray-500 font-bold mt-10">Selo</p>
+      <p className="text-[#a2a3a5] font-bold mt-10">Selo</p>
       {labels.map(({ label: lbl, checked }, idx) => (
         <label key={idx} className="items-center mt-3 block">
           <input
@@ -22,7 +22,7 @@ export default function Labels() {
             onChange={() => updateLabel({ label: lbl, checked: !checked })}
             className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded focus:ring-0 cursor-pointer`}
           />
-          <span className="ml-2 text-gray-700 capitalize">{cores[lbl]}</span>
+          <span className="ml-2 text-[#B4BABC] capitalize">{cores[lbl]}</span>
         </label>
       ))}
     </React.Fragment>
