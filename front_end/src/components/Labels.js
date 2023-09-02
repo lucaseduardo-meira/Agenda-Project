777dfd54f1507bb40751b616dfbcf7a214781231
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2023-09-02 15:12:50 -0300

    Update Labels.js

--- front_end/src/components/Labels.js
+++ front_end/src/components/Labels.js
@@ -10,6 +10,14 @@ export default function Labels() {
     gray: "Cinza",
     green: "Verde",
   };
+  const hex = {
+    indigo: "#6366f1",
+    red: "#ef4444",
+    blue: "#3b82f6",
+    purple: "#a855f7",
+    gray: "#6b7280",
+    green: "#84cc16",
+  };
   const { labels, updateLabel } = useContext(GlobalContext);
   return (
     <React.Fragment>
@@ -20,7 +28,7 @@ export default function Labels() {
             type="checkbox"
             checked={checked}
             onChange={() => updateLabel({ label: lbl, checked: !checked })}
-            className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded focus:ring-0 focus:ring-offset-0 focus:outline-none cursor-pointer`}
+            className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded border-${lbl}-500 border-[medium] bg-[#191919] focus:ring-0 focus:ring-offset-0 focus:outline-none cursor-pointer`}
           />
           <span className="ml-2 text-[#B4BABC] capitalize">{cores[lbl]}</span>
         </label>
